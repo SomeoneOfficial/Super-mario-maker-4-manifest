72a1d193b304952ba53bd6422bde0c51a4627686
SomeoneOfficial <aprusjeet2011@gmail.com>
2025-04-04 17:07:20 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'Super Mario Maker 4 - benjammo-v1-manifest test';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
